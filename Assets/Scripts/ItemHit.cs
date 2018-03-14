7ef41de761286f1ec3a37c9a469c009749ba1893
Julia <juliamjuliano@gmail.com>
2018-03-14 07:55:17 -0700

    Starting to create levels

---
+++ Assets/Scripts/ItemHit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ItemHit : MonoBehaviour, IInputClickHandler{
+
+	// Called by GazeGestureManager when the user performs a Select gesture
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+
+		if (!this.GetComponent<Rigidbody>())
+		{
+			var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+			rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+		}
+	}
+}
+
